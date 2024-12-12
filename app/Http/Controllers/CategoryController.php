435beb4a50f61eb3f0145eb1ce95d3c20ae29408
Gabe <gabessj4@gmail.com>
2024-12-11 23:29:34 -0400

    added some other category files, the controller and the database migration for categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* CategoryController.php
+* Author: Gabriel Ponce
+* Date: 2024-12-05
+**/
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function createCategory(Request $request)
+    {
+        $data = $request->validate(
+            [
+                'name' => 'required|string',
+            ]
+        );
+
+        $data['name'] = strip_tags($data['name']);
+        Category::create($data);
+        return redirect('/categories');
+    }
+
+    public function editCategory(Category $category)
+    {
+        return view('categoryEdit', ['category' => $category]);
+    }
+
+    public function editedCategory(Request $request, Category $category)
+    {
+        $data = $request->validate(
+            [
+                'name' => 'required|string',
+            ]
+        );
+
+        $data['name'] = strip_tags($data['name']);
+        $category->update($data);
+        return redirect('/categories');
+    }
+}
